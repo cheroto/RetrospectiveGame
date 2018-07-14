b4e6408da57a4e2d7feee01441ce41da78c26c6b
Pedro <pedro.cheroto@gmail.com>
2018-07-14 11:19:07 -0400

    Wife interrupted my work, have to stop, no major updates

--- RetrospectiveGame/CombatHandler.cs
+++ RetrospectiveGame/CombatHandler.cs
@@ -8,6 +8,7 @@ namespace RetrospectiveGame
 {
     class CombatHandler
     {
+        private const int maxValue = 20;
         private readonly IDiceRoller diceRoller = new DiceRoller();
         public void AttackRound(ICharacter attacker, ICharacter defender)
         {
@@ -18,12 +19,32 @@ namespace RetrospectiveGame
 
         private string CheckIfAttackSuccessful(ICharacter attacker, ICharacter defender)
         {
+            var attackSuccessfullness = "Miss!";
             var attackBonus = (attacker.Strength - 10) / 2;
             var modifier = attacker.Modifier;
             var diceRoll = diceRoller.RollDice();
             int totalAttackingPower = GetTotalAttackingPower(attackBonus, modifier, diceRoll);
 
-            return "";
+            if (diceRoll == maxValue)
+            {
+                attackSuccessfullness = CheckForCritical(defender.Constitution, attackSuccessfullness, attackBonus, modifier);
+            }
+            else if (diceRoll > defender.Constitution)
+            {
+                attackSuccessfullness = "Hit!";
+            }
+            return attackSuccessfullness;
+        }
+
+        private string CheckForCritical(int defense, string attackSuccessfullness, int attackBonus, int modifier)
+        {
+            var diceRollForCritical = diceRoller.RollDice();
+            var criticalTestValue = GetTotalAttackingPower(attackBonus, modifier, diceRollForCritical);
+            if (criticalTestValue > defense)            
+                attackSuccessfullness = "Critical!";            
+            else
+                attackSuccessfullness = "Hit!";
+            return attackSuccessfullness;
         }
 
         private int GetTotalAttackingPower(int attackBonus, int modifier, int diceRoll)
