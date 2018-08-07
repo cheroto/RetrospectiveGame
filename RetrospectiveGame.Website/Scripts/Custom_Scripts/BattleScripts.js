77734c333d245caac6c7801c621b2fe0b52f10af
Pedro <pedro.cheroto@gmail.com>
2018-08-06 21:42:51 -0400

    Switched over to Bootstrap progressbar

--- RetrospectiveGame.Website/Scripts/Custom_Scripts/BattleScripts.js
+++ RetrospectiveGame.Website/Scripts/Custom_Scripts/BattleScripts.js
@@ -12,9 +12,25 @@
             var $target = $($form.attr("data-rg-target"));
             $target.replaceWith(data);
             $("#startButton").val("Next Round");
+            UpdateProgressBarColor();
             console.log(data);
         });
         return false;
+
+
+        function UpdateProgressBarColor() {
+            $(".progress-bar").each(function () {
+                if ($(this).attr("aria-valuenow") < 50) {
+                    $(this).removeClass("progress-bar-success");
+                    $(this).addClass("progress-bar-warning");
+                }
+                if ($(this).attr("aria-valuenow") < 30) {
+                    $(this).removeClass("progress-bar-success");
+                    $(this).removeClass("progress-bar-warning");
+                    $(this).addClass("progress-bar-danger");
+                }
+            })
+        }
     };
 
     $("form[data-rg-ajax='true']").submit(ajaxFormSubmit);
